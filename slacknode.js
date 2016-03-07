608dd0c61459bdad2246e171e2391f74cb8cc749
doctorparadox <livemind@gmail.com>
2016-03-07 02:13:37 -0800

    stormy weather

--- slacknode.js
+++ slacknode.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 9001));
+
 app.get('/', function(req, res) {
 	res.send('yo yo yo what's tha scenario');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
 });
