4ef4460a842e2c5ce63e1739b2008535d17dc247
doctorparadox <livemind@gmail.com>
2016-03-07 01:48:41 -0800

    & now for something completely different

--- slacknode.js
+++ slacknode.js
@@ -1,9 +1,6 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req,res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello world!');
-}).listen(3000);
-
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
-
+app.get('/', function(req, res) {
+	res.send('yo yo yo what's tha scenario');
+});
