04de12e6658d0d929c1da3fab340257aa4bb32ea
doctorparadox <livemind@gmail.com>
2016-03-07 03:25:07 -0800

    pears in season

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var express = require('express');
 var app = express();
-var url = require('url');
-var request = require('request');
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 9001));
 
